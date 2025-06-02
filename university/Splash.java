4916e597a1dcaa8b884736f29088e4e1aed857f2
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-06-02 19:03:16 +0300

    default constructor added, and extend JFrame class to create a frame screen, importing from swing.

--- university/Splash.java
+++ university/Splash.java
@@ -1,4 +1,11 @@
-public class Splash {
+import javax.swing.*;
+
+//to create a frame screen
+public class Splash extends JFrame{
+
+    Splash(){
+
+    }
 
     public static void main(String[] args) {
         new Splash();
