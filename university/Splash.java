416c29fbbd8f34dc3129e58d7933a7ef888508d1
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-06-02 19:17:36 +0300

    setVisible added to make frame visible on screen, setSize and setLocation added to set the size and exact location of the frame on the screen using parameters.

--- university/Splash.java
+++ university/Splash.java
@@ -5,6 +5,12 @@ public class Splash extends JFrame{
 
     Splash(){
 
+        setLocation(450,150);
+        //to set the sixe of the frame
+        setSize(1000,700);
+        //the screen is not visible so set to true to make it visible
+		setVisible(true);
+
     }
 
     public static void main(String[] args) {
