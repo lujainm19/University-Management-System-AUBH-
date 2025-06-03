30df32d40c8b02e80fd4feb3a853fe758c4272fd
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-06-03 22:33:11 +0300

    Icons folder with all the jpg images pasted in the university management system package. ImageIcon object created toad images from icons in the frame, use static method getSystemResource and add address of image, create JLabel object and use add function from JFrame class, to give website background image.

--- university/Splash.java
+++ university/Splash.java
@@ -4,13 +4,18 @@ import javax.swing.*;
 public class Splash extends JFrame{
 
     Splash(){
+        
+        //to add a picture to background of frame create object of it and add 
+		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/aubh background.jpg"));
+        JLabel image = new JLabel(i1);
+		add(image);
 
+        //to set the location of the frame on the screen
         setLocation(450,150);
         //to set the sixe of the frame
         setSize(1000,700);
         //the screen is not visible so set to true to make it visible
 		setVisible(true);
-
     }
 
     public static void main(String[] args) {
