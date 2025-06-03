2155121b3e659462c3f5ea2f478f37bfa7a91954
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-06-04 00:11:11 +0300

    Extend JFrame, import java swingto create frame setSize and setLocation and setVisible to true, to appear in center of screen.

--- university/Login.java
+++ university/Login.java
@@ -1,3 +1,15 @@
-public class Login {
+import javax.swing.*;
 
+public class Login  extends JFrame{
+
+    Login () {
+        setSize(600, 300);
+        setLocation(700, 350);
+        setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+        new Login();
+    }
 }
