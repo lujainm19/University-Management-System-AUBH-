ffa5e2101cef171e843d2108edb224aaa36dc4b2
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-06-03 22:51:05 +0300

    To make frame increase in size when run, move setVisible up, and for loop added for setLocation and setSize, and values changed according to how and where frame wanted to open on screen.

--- university/Splash.java
+++ university/Splash.java
@@ -9,17 +9,21 @@ public class Splash extends JFrame{
         //to add a picture to background of frame create object of it and add 
 		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/aubh.jpg"));
         //to scale the image properly on the frame
-		Image i2 = i1.getImage().getScaledInstance(1000, 650, Image.SCALE_DEFAULT);
+		Image i2 = i1.getImage().getScaledInstance(1500, 650, Image.SCALE_DEFAULT);
 		ImageIcon i3 = new ImageIcon(i2);
 		JLabel image = new JLabel(i3);
 		add(image);
 
-        //to set the location of the frame on the screen
-        setLocation(450,150);
-        //to set the sixe of the frame
-        setSize(1000,700);
-        //the screen is not visible so set to true to make it visible
+        //the screen is not visible so add this to make it visible
 		setVisible(true);
+		
+		// for loop to make frame small to big(increase in size) and adjust from up down, right left
+		for (int i = 0; i <= 500; i+= 10) {
+			//to set the exact origin or place the frame opens on the screen x,y 450, 150
+			setLocation(900-(i-50), 300-(i-350));
+			//to automatically set the length, width of the framing without having to extend it manually 1000, 700
+			setSize(2*i, i+200);
+		}
     }
 
     public static void main(String[] args) {
