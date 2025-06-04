6bbc5abb56b1f0fcbc01864f33eda63943c2bf15
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-06-04 21:39:39 +0300

    setSize of frame, setVisible to true, copy code to insert image changing image address, and getScaledInstance, import awt package. Ensure setVisible is last statement.

--- university/Project.java
+++ university/Project.java
@@ -1,8 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Project extends JFrame{
 
     Project() {
+        setSize(1925,1050);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/aubh.jpg"));
+		Image i2 = i1.getImage().getScaledInstance(1925, 975, Image.SCALE_DEFAULT);
+		ImageIcon i3 = new ImageIcon(i2);
+		JLabel image = new JLabel(i3);
+		add(image);
+
+        setVisible(true);
 
     }
 
