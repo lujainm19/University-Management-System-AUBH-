61c4e430a2d3201790f6065db318f13992a7dc4f
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-06-07 18:44:51 +0300

    MySQL linked and the MySQL driver registered using class Class, ForName, and driver of MySQL driver as the argument.

--- universitymanagementsystem/Conn.java
+++ universitymanagementsystem/Conn.java
@@ -1,9 +1,10 @@
-package src.universitymanagementsystem;
+package universitymanagementsystem;
 
 public class Conn {
 
     Conn() {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
         } catch (Exception e) {
             e.printStackTrace();
