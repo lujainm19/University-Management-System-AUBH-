981d7f7d7ac954c515b0fef1fe347fe25a625f32
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-06-12 18:15:24 +0300

    Font size and type of heading set using setFont function.

--- universitymanagementsystem/AddStudent.java
+++ universitymanagementsystem/AddStudent.java
@@ -1,4 +1,6 @@
 package universitymanagementsystem;
+import java.awt.Font;
+
 import javax.swing.*;
 
 //to create a frame
@@ -13,6 +15,7 @@ public class AddStudent extends JFrame{
         
         JLabel heading = new JLabel("New Student Details");
         heading.setBounds(300, 70, 500, 50);
+        heading.setFont(new Font("serif", Font.BOLD, 30));
         add(heading);
 
         setVisible(true);
