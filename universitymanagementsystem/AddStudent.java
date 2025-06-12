1a4eb492de30e753f083d639d07ad15765f9aea5
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-06-12 18:40:04 +0300

    Globally declaring the variable tfname, and JTextField object created, setting its bounds, to take input from user. Displayed using add function.

--- universitymanagementsystem/AddStudent.java
+++ universitymanagementsystem/AddStudent.java
@@ -6,6 +6,8 @@ import javax.swing.*;
 //to create a frame
 public class AddStudent extends JFrame{
 
+    JTextField tfname;
+
     AddStudent(){
         //frame size, location and visibility
         setSize(900, 700);
@@ -13,15 +15,21 @@ public class AddStudent extends JFrame{
 
         setLayout(null);
         
+        //New Student Details heading
         JLabel heading = new JLabel("New Student Details");
         heading.setBounds(310, 30, 500, 50);
         heading.setFont(new Font("serif", Font.BOLD, 30));
         add(heading);
         
+        //Name: heading
         JLabel lblname = new JLabel("Name:");
         lblname.setBounds(50, 150, 100, 30);
         lblname.setFont(new Font("serif", Font.BOLD, 20));
         add(lblname);
+
+        tfname = new JTextField();
+        tfname.setBounds(200, 150, 150, 30);
+        add(tfname);
 
         setVisible(true);
 
