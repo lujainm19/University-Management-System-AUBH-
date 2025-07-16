e39a47f868d90ca7a38b014ec2808877f8c3d61a
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-16 20:57:47 +0300

    Initial commit at correct root level

---
+++ src/universitymanagementsystem/Login.java
@@ -0,0 +1,100 @@
+package universitymanagementsystem;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class Login  extends JFrame implements ActionListener{
+
+    JButton Login, Cancel;
+    JTextField tfusername, tfpassword;
+
+    Login () {
+
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        JLabel lblusername = new JLabel("Username: ");
+        lblusername.setBounds(40, 20, 100, 20);
+        add(lblusername);
+
+        tfusername = new JTextField();
+        tfusername.setBounds(150, 20, 150,20);
+        add(tfusername);
+
+        JLabel lblpassword = new JLabel("Password: ");
+        lblpassword.setBounds(40, 70, 100, 20);
+        add(lblpassword);
+
+        tfpassword = new JPasswordField();
+        tfpassword.setBounds(150, 70, 150, 20);
+        add(tfpassword);
+
+        Login = new JButton("Login");
+        Login.setBounds(40, 140, 120, 30);
+        Login.setBackground(Color.BLACK);
+        Login.setForeground(Color.WHITE);
+        Login.addActionListener((this));
+        Login.setFont(new Font("Tahoma", Font.BOLD, 15));
+        add(Login);
+
+        Cancel = new JButton("Cancel");
+        Cancel.setBounds(180, 140, 120, 30);
+        Cancel.setBackground(Color.BLACK);
+        Cancel.setForeground(Color.WHITE);
+        Cancel.addActionListener((this));
+        Cancel.setFont(new Font("Tahoma", Font.BOLD, 15));
+        add(Cancel);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/second.jpg"));
+		Image i2 = i1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+		ImageIcon i3 = new ImageIcon(i2);
+		JLabel image = new JLabel(i3);
+        image.setBounds(350, 0, 200, 200);
+		add(image);
+
+        setSize(600, 300);
+        setLocation(700, 350);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == Login) {
+            String username = tfusername.getText();
+            String password = tfpassword.getText();
+
+            //checks if a user's username and password exist in the login table of the database.
+            String query = "select * from login where username='"+username+"' and password='"+password+"'";
+        
+             //Mysql is an excel entity query so chances of error so error handling
+            try {
+                Conn c = new Conn();
+                ResultSet  rs = c.s.executeQuery(query);
+
+                if (rs.next()) {
+                    setVisible(false);
+                    new Project();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid username or password");
+                    setVisible(false);
+
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        
+        } else if (ae.getSource() == Cancel) {
+            setVisible(false);
+        }
+       
+    }
+
+
+    public static void main(String[] args) {
+        new Login();
+    }
+
+}
---
+++ src/universitymanagementsystem/Splash.java
@@ -0,0 +1,49 @@
+package universitymanagementsystem;
+
+import javax.swing.*;
+import java.awt.*;
+
+//to create a frame screen
+public class Splash extends JFrame implements Runnable{
+    Thread t;
+    Splash(){
+        
+        //to add a picture to background of frame create object of it and add 
+		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/project.png"));
+        //to scale the image properly on the frame
+		Image i2 = i1.getImage().getScaledInstance(1000, 675, Image.SCALE_DEFAULT);
+		ImageIcon i3 = new ImageIcon(i2);
+		JLabel image = new JLabel(i3);
+		add(image);
+
+        t = new Thread(this);
+        t.start();//the start method internally calls the run method 
+
+        //the screen is not visible so add this to make it visible
+		setVisible(true);
+		
+		// for loop to make frame small to big(increase in size) and adjust from up down, right left
+		for (int i = 0; i <= 500; i+= 10) {
+			//to set the exact origin or place the frame opens on the screen x,y 450, 150
+			setLocation(900-(i-50), 300-(i-350));
+			//to automatically set the length, width of the framing without having to extend it manually 1000, 700
+			setSize(2*i, i+200);
+		}
+    }
+
+    public void run() {
+        try {
+			Thread.sleep(7000);
+			setVisible(false);
+			
+			//next frame
+            new Login();
+		}catch (Exception e) {
+			
+		}
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
