1acb82e26d72d04017428731510d162383adb9cb
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-16 18:55:12 +0300

    Made into a frame, extends JFrame,implement swing package. setsize, location, background, implement awt, and set visible to true. setLayout to null.

--- universitymanagementsystem/StudentDetails.java
+++ universitymanagementsystem/StudentDetails.java
@@ -1,8 +1,20 @@
 package universitymanagementsystem;
 
-public class StudentDetails {
+import java.awt.Color;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StudentDetails extends JFrame{
     StudentDetails(){
+
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
         
+        setSize(1000,700);
+        setLocation(450,150);
+        setVisible(true);
+
     }
 
     public static void main(String[] args) {
