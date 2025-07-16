19fb7d7080bacd71087a57a3b7835921bf868891
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-16 18:45:30 +0300

    To show details under View details, StudentDetails class created, with main method, class object and default constructor.

---
+++ universitymanagementsystem/StudentDetails.java
@@ -0,0 +1,12 @@
+package universitymanagementsystem;
+
+public class StudentDetails {
+    StudentDetails(){
+        
+    }
+
+    public static void main(String[] args) {
+        new StudentDetails();
+    }
+
+}
