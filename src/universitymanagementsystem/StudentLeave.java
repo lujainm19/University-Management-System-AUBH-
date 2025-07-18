bd64e61675c2c51dc7404664b19c90d158e9d30f
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-18 13:40:36 +0300

    Apply Leave heading added by creating JLabel object with bounds and font and displayed using add function.

--- src/universitymanagementsystem/StudentLeave.java
+++ src/universitymanagementsystem/StudentLeave.java
@@ -1,8 +1,24 @@
 package universitymanagementsystem;
 
-public class StudentLeave {
+import javax.swing.*;
+import java.awt.*;
 
+public class StudentLeave extends JFrame {
     StudentLeave(){
+
+        setSize(650, 700);
+        setLocation(650, 200);
+        setLayout(null);
+
+        getContentPane().setBackground(Color.WHITE);
+
+        //Apply Leave (Student) heading
+        JLabel heading = new JLabel("Apply Leave (Student)");
+        heading.setBounds(40, 50, 300, 30);
+        heading.setFont(new Font("Tahoma", Font.BOLD, 20));
+        add(heading);
+
+        setVisible(true);
 
     }
 
