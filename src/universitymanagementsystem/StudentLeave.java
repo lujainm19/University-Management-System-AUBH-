3c4f9fc14306cee1de3a7819845a03b40d5e62a3
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-18 13:49:09 +0300

    Code for student ID drop down copied from StudentDetails and pasted changing font size, type, object name.

--- src/universitymanagementsystem/StudentLeave.java
+++ src/universitymanagementsystem/StudentLeave.java
@@ -2,8 +2,12 @@ package universitymanagementsystem;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.*;
 
 public class StudentLeave extends JFrame {
+
+    Choice cstdIDno; 
+
     StudentLeave(){
 
         setSize(650, 700);
@@ -17,6 +21,25 @@ public class StudentLeave extends JFrame {
         heading.setBounds(40, 50, 300, 30);
         heading.setFont(new Font("Tahoma", Font.BOLD, 20));
         add(heading);
+
+        JLabel lblIDno = new JLabel("Search by Student ID");
+        lblIDno.setBounds(60, 100, 200, 20);
+        lblIDno.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        add(lblIDno);
+
+        cstdIDno = new Choice();
+        cstdIDno.setBounds(60, 130, 200, 20);
+        add(cstdIDno);
+
+        try {
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from student");
+            while (rs.next()) {
+                cstdIDno.add(rs.getString("stdID"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         setVisible(true);
 
