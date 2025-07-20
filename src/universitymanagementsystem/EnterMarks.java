f0f06e612daf4d7e6b996fcaccdac3ec7ff5b62f
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-20 15:55:25 +0300

    Heading and Select Student ID heading code copied from UpdateStudent.

--- src/universitymanagementsystem/EnterMarks.java
+++ src/universitymanagementsystem/EnterMarks.java
@@ -2,6 +2,7 @@ package universitymanagementsystem;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.*;
 
 public class EnterMarks extends JFrame{
 
@@ -13,12 +14,39 @@ public class EnterMarks extends JFrame{
 
         getContentPane().setBackground(Color.WHITE);
 
+        //exam image 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/exam.jpg"));
 		Image i2 = i1.getImage().getScaledInstance(400, 300, Image.SCALE_DEFAULT);
 		ImageIcon i3 = new ImageIcon(i2);
 		JLabel image = new JLabel(i3);
         image.setBounds(570, 60, 450, 350);
 		add(image);
+
+        //New Student Details heading
+        JLabel heading = new JLabel("Update Student Details");
+        heading.setBounds(50, 10, 500, 50);
+        heading.setFont(new Font("Tahoma", Font.ITALIC, 35));
+        add(heading);
+
+        //Search by Student ID heading
+        JLabel lblstdIDnumber = new JLabel("Select Student ID");
+        lblstdIDnumber.setBounds(50, 100, 200, 20);
+        lblstdIDnumber.setFont(new Font("serif", Font.PLAIN, 20));
+        add(lblstdIDnumber);
+
+        cstdIDno = new Choice();
+        cstdIDno.setBounds(250, 100, 200, 20);
+        add(cstdIDno);
+
+        try {
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from student");
+            while (rs.next()) {
+                cstdIDno.add(rs.getString("stdID"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         setVisible(true);
 
