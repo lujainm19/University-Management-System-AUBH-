af8a0b6b505a83b63e3c9da4eb303009fe0aae45
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-20 15:42:46 +0300

    Made into a frame with size, location, background color and visibility set to true.

--- src/universitymanagementsystem/EnterMarks.java
+++ src/universitymanagementsystem/EnterMarks.java
@@ -1,9 +1,20 @@
 package universitymanagementsystem;
 
-public class EnterMarks {
+import javax.swing.*;
+import java.awt.*;
+
+public class EnterMarks extends JFrame{
 
     EnterMarks(){
-        
+
+        setSize(1100, 600);
+        setLocation(400,250);
+        setLayout(null);
+
+        getContentPane().setBackground(Color.WHITE);
+
+        setVisible(true);
+
     }
 
 public static void main(String[] args) {
