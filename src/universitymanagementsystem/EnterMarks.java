b2cbff92d716e70eb01e4692dbe23a4955619c69
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-20 15:51:16 +0300

    Code to add image copied from project. Image address. size changed, and bounds added.

--- src/universitymanagementsystem/EnterMarks.java
+++ src/universitymanagementsystem/EnterMarks.java
@@ -13,6 +13,13 @@ public class EnterMarks extends JFrame{
 
         getContentPane().setBackground(Color.WHITE);
 
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/exam.jpg"));
+		Image i2 = i1.getImage().getScaledInstance(400, 300, Image.SCALE_DEFAULT);
+		ImageIcon i3 = new ImageIcon(i2);
+		JLabel image = new JLabel(i3);
+        image.setBounds(570, 60, 450, 350);
+		add(image);
+
         setVisible(true);
 
     }
