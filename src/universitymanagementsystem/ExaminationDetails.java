87381000f242d6ff0837b252bb0ab467e1fc8bfe
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-21 22:04:13 +0300

    Submit and Cancel button code copied from EnterMarks with necessary changes, submit changed to result.

--- src/universitymanagementsystem/ExaminationDetails.java
+++ src/universitymanagementsystem/ExaminationDetails.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class ExaminationDetails  extends JFrame{
 
     JTextField search;
+    JButton cancel, submit;
 
     ExaminationDetails() {
         setSize(1000, 550);
@@ -27,6 +28,22 @@ public class ExaminationDetails  extends JFrame{
         search.setBounds(80, 90, 200, 30);
         search.setFont(new Font("Tahoma", Font.PLAIN, 18));
         add(search);
+
+        submit = new JButton("View Result");
+        submit.setBounds(300,90,150,30);
+        submit.setBackground(Color.BLACK);
+        submit.setForeground(Color.WHITE);
+        //submit.addActionListener(this);
+        submit.setFont(new Font("Tahoma", Font.BOLD, 15));
+        add(submit);
+
+        cancel = new JButton("Back");
+        cancel.setBounds(440,90,150,30);
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+        //cancel.addActionListener(this);
+        cancel.setFont(new Font("Tahoma", Font.BOLD, 15));
+        add(cancel);
 
         setVisible(true);
     }
