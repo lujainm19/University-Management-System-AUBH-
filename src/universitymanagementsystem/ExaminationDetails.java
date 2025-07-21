7eaf808dc135559bdf23fd196b157b8f7516e70d
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-21 21:51:51 +0300

    Heading code copied from EnterMarks with background color.

--- src/universitymanagementsystem/ExaminationDetails.java
+++ src/universitymanagementsystem/ExaminationDetails.java
@@ -1,12 +1,24 @@
 package universitymanagementsystem;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ExaminationDetails  extends JFrame{
 
     ExaminationDetails() {
         setSize(1000, 550);
         setLocation(450, 250);
+
+
+        setLayout(null);
+
+        getContentPane().setBackground(Color.WHITE);
+
+        //View Results
+        JLabel heading = new JLabel("View Results");
+        heading.setBounds(80, 15, 400, 50);
+        heading.setFont(new Font("Tahoma", Font.BOLD, 24));
+        add(heading);
 
         setVisible(true);
     }
