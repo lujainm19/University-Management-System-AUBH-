bf291aa3bad79297dafad5c957a71d3cf45c9fe7
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-21 21:39:16 +0300

    New class ExaminationDetails created with main method, object of class and default constructor.

---
+++ src/universitymanagementsystem/ExaminationDetails.java
@@ -0,0 +1,15 @@
+package universitymanagementsystem;
+
+public class ExaminationDetails {
+
+    ExaminationDetails() {
+        
+    }
+
+
+    public static void main(String[] args) {
+        new ExaminationDetails();
+    }
+}
+
+
