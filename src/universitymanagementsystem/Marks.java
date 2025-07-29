8061f1b9aef2b62128e7bfaf145781e0221098c8
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-30 00:31:06 +0300

    New Marks class created with parameterized constructor, main method calling the parameterized constructor.

---
+++ src/universitymanagementsystem/Marks.java
@@ -0,0 +1,15 @@
+package universitymanagementsystem;
+
+public class Marks {
+
+    String IDno;
+
+    Marks (String IDno) {
+        this.IDno = IDno;
+
+    }
+
+    public static void main(String[] args) {
+        new Marks("");
+    }
+}
