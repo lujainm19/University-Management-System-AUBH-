1c1186bb360840e7da8cdb040bf964586bfbb585
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-30 00:51:12 +0300

    Result of Examination heading added.

--- src/universitymanagementsystem/Marks.java
+++ src/universitymanagementsystem/Marks.java
@@ -18,9 +18,15 @@ public class Marks extends JFrame{
 
         //American University of Bahrain heading
         JLabel heading = new JLabel("American University of Bahrain");
-        heading.setBounds(100, 10, 500, 25);
+        heading.setBounds(110, 10, 500, 25);
         heading.setFont(new Font("Tahoma", Font.BOLD, 20));
         add(heading);
+
+        //Result of Examination heading
+        JLabel subheading = new JLabel("Result of Examination");
+        subheading.setBounds(160, 50, 500, 20);
+        subheading.setFont(new Font("Tahoma", Font.BOLD, 18));
+        add(subheading);
 
         setVisible(true);
 
