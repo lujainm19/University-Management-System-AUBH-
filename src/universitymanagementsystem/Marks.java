9aae58fdfc0527262105940c8a232fa34606bdaf
lujainm19 <157721310+lujainm19@users.noreply.github.com>
2025-07-30 00:47:01 +0300

    Heading code copied and pasted with necessary changes (American University of Bahrain).

--- src/universitymanagementsystem/Marks.java
+++ src/universitymanagementsystem/Marks.java
@@ -1,6 +1,7 @@
 package universitymanagementsystem;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Marks extends JFrame{
 
@@ -11,6 +12,16 @@ public class Marks extends JFrame{
 
         setSize(550,650);
         setLocation(600,200);
+        setLayout(null);
+
+        getContentPane().setBackground(Color.WHITE);
+
+        //American University of Bahrain heading
+        JLabel heading = new JLabel("American University of Bahrain");
+        heading.setBounds(100, 10, 500, 25);
+        heading.setFont(new Font("Tahoma", Font.BOLD, 20));
+        add(heading);
+
         setVisible(true);
 
     }
